f4122777ed559c0f5db0caa8c4a013c29c3fb56b
ruKyzhc <ruKyzhc@c9c5b5aa-0367-836e-72da-8aa6a4002a05>
2010-06-08 08:58:02 +0000

    edit browser.js

---
+++ trunk/jfcrawler/src/org/thuir/forum/js/browser.js
@@ -0,0 +1,46 @@
+var printer = "";
+reset = function() { printer = ""; }
+
+//navigator
+function _NAVIGATOR() {
+	this.userAgent = "jfcrawler(THUIR-bot)";
+}
+var navigator      = new _NAVIGATOR();
+
+//window
+function _WINDOW() {
+	this.self = this;
+	this.top  = this;
+}
+var window = new _WINDOW();
+
+var top    = window;
+var self   = window;
+
+//document
+function _DOCUMENT(_window, _obj, _cookie) {
+	this.cookie    = _cookie;
+	this.frame     = _obj;
+	this.frames    = _obj;
+	this.history   = _obj;
+	
+	this.document  = this;
+	this.length    = 0;
+	
+	this.defaultStatus = "";
+	this.location  = "";
+	this.name      = "";
+	this.status    = "";
+	
+	this.opener    = _window;
+	this.parent    = _window;
+	this.self      = _window;
+	this.top       = _window;
+	this.window    = _window;
+	
+	this.getElementById
+		= function(id) {};
+	this.write
+		= function(s) {printer+=(s+'\n');};
+}
+var document      = new _DOCUMENT(window, new Object(), new Object());
